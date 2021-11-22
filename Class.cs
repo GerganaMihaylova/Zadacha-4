55f6ffb80fce9172ad874e842e8228600aad58ce
Gergana Mihaylova <82368057+GerganaMihaylova@users.noreply.github.com>
2021-11-22 13:18:40 +0200

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace _Appointment.Doctor.Models
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+    
+}
